3457abf5002417c0a5653301992ee29a19ed320e
Mark Webb-Johnson <mark@openvehicles.com>
2025-07-04 21:10:53 +0800

    Make project web-safe (disable map location on web)

---
+++ components/ui/Map.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import MapView, { Marker } from "react-native-maps";
+
+const INITIAL_REGION = {
+  latitude: 52.5,
+  longitude: 19.2,
+  latitudeDelta: 8.5,
+  longitudeDelta: 8.5,
+};
+
+export function Map() {
+  return (
+      <MapView
+       initialRegion={INITIAL_REGION}
+       style={{ flex: 1 }}>
+        <Marker coordinate={{ latitude: 52.4, longitude: 18.7 }} />
+        <Marker coordinate={{ latitude: 52.1, longitude: 18.4 }} />
+        <Marker coordinate={{ latitude: 52.6, longitude: 18.3 }} />
+        <Marker coordinate={{ latitude: 51.6, longitude: 18.0 }} />
+        <Marker coordinate={{ latitude: 53.1, longitude: 18.8 }} />
+        <Marker coordinate={{ latitude: 52.9, longitude: 19.4 }} />
+        <Marker coordinate={{ latitude: 52.2, longitude: 21 }} />
+        <Marker coordinate={{ latitude: 52.4, longitude: 21 }} />
+        <Marker coordinate={{ latitude: 51.8, longitude: 20 }} />
+      </MapView>
+  );
+}
---
+++ components/ui/Map.web.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View } from "react-native";
+import { Text } from "react-native-paper";
+
+export function Map() {
+  return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Sorry, map not available on web</Text>
+      </View>
+  );
+}
