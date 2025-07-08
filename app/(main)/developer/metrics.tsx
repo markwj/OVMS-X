0aeae7eb74b8349439e7f34fd93ef380e0e848b5
Mark Webb-Johnson <mark@openvehicles.com>
2025-07-08 21:39:02 +0800

    Fixed to developer menu

--- app/(main)/developer/metrics.tsx
+++ app/(main)/developer/metrics.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { ScrollView } from 'react-native';
+import React, { useEffect, useState, useLayoutEffect } from "react";
+import { ScrollView, View } from 'react-native';
 import { metricsAllKeysSelector, metricsSlice, metricsAllValuesSelector } from "@/store/metricsSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { useTranslation } from 'react-i18next';
 import MetricTable from "@/components/ui/MetricTable";
-import { Menu, Button, Icon, Portal, Dialog, Text } from "react-native-paper";
+import { Menu, Button, IconButton, Portal, Dialog, Text } from "react-native-paper";
 import { useNavigation } from "expo-router";
 
 //@ts-ignore
@@ -14,29 +14,31 @@ export default function DeveloperScreen() {
   const { t } = useTranslation();
 
   const [visible, setVisible] = useState(false);
-
-  const closeMenu = () => setVisible(false);
+  const openMenu = () => { console.log('openMenu'); setVisible(true); }
+  const closeMenu = () => { console.log('closeMenu'); setVisible(false); }
+  console.log('visible', visible)
 
   let keys = useSelector(metricsAllKeysSelector)
   let values = useSelector(metricsAllValuesSelector)
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     navigation.setOptions({
-      headerRight: () => <Button onPress={() => setVisible(true)} children={<Icon size={20} source={"cog"} />} />
+      headerRight: () =>
+        <Menu
+          visible={visible}
+          onDismiss={closeMenu}
+          anchor={<IconButton onPress={openMenu} size={20} icon={'dots-vertical'} />}
+          anchorPosition="bottom">
+          <Menu.Item leadingIcon="refresh" onPress={() => { dispatch(metricsSlice.actions.resetToStandardMetrics()); closeMenu(); }} title={t("Reset standard metrics")} />
+          <Menu.Item leadingIcon="eraser" onPress={() => { dispatch(metricsSlice.actions.clearAll()); closeMenu(); }} title={t("Clear all metrics")} />
+          <Menu.Item leadingIcon="delete" onPress={() => { dispatch(metricsSlice.actions.deleteAll()); closeMenu(); }} title={t("Delete all metrics")} />
+        </Menu>
     })
-  }, [navigation])
+  }, [navigation, visible])
 
   return (
     <ScrollView>
-      <Menu
-        visible={visible}
-        onDismiss={() => setVisible(false)}
-        anchor={<MetricTable metrics={values} metricKeys={keys} />}
-      >
-        <Menu.Item leadingIcon="refresh" onPress={() => {dispatch(metricsSlice.actions.resetToStandardMetrics()); closeMenu();}} title={t("Reset to standard metrics")} />
-        <Menu.Item leadingIcon="eraser" onPress={() => {dispatch(metricsSlice.actions.clearAll()); closeMenu(); }} title={t("Clear all metrics")} />
-        <Menu.Item leadingIcon="delete" onPress={() => {dispatch(metricsSlice.actions.deleteAll()); closeMenu(); }} title={t("Delete all metrics")} />
-      </Menu>
+      <MetricTable metrics={values} metricKeys={keys} />
     </ScrollView>
   );
 }
