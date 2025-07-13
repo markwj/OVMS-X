8db0eca4b5df2cf2f62e3d6dda648fd530c8f1ec
Mark Webb-Johnson <mark@openvehicles.com>
2025-07-13 16:53:03 +0800

    Fix image paths

--- components/ui/VehicleImages.tsx
+++ components/ui/VehicleImages.tsx
@@ -5,48 +5,48 @@ import { VehicleImage } from "@/store/vehiclesSlice"
 export const VehicleTypes = {
   'ampera': {
     name: 'Opel Ampera-e',
-    side: require('@/assets/carimages/ampera-side-base.png'),
-    sideMask: require('@/assets/carimages/ampera-side-mask.png'),
-    top: require('@/assets/carimages/ampera-top-base.png'),
-    topMask: require('@/assets/carimages/ampera-top-mask.png'),
-    map: require('@/assets/carimages/ampera-map-base.png'),
-    mapMask: require('@/assets/carimages/ampera-map-mask.png')
+    side: require('@/assets/carimages/ampera_side_base.png'),
+    sideMask: require('@/assets/carimages/ampera_side_mask.png'),
+    top: require('@/assets/carimages/ampera_top_base.png'),
+    topMask: require('@/assets/carimages/ampera_top_mask.png'),
+    map: require('@/assets/carimages/ampera_map_base.png'),
+    mapMask: require('@/assets/carimages/ampera_map_mask.png')
   },
   'roadster': {
     name: 'Tesla Roadster (2008-2012)',
-    side: require('@/assets/carimages/roadster-side-base.png'),
-    sideMask: require('@/assets/carimages/roadster-side-mask.png'),
-    top: require('@/assets/carimages/roadster-top-base.png'),
-    topMask: require('@/assets/carimages/roadster-top-mask.png'),
-    map: require('@/assets/carimages/roadster-map-base.png'),
-    mapMask: require('@/assets/carimages/roadster-map-mask.png')
+    side: require('@/assets/carimages/roadster_side_base.png'),
+    sideMask: require('@/assets/carimages/roadster_side_mask.png'),
+    top: require('@/assets/carimages/roadster_top_base.png'),
+    topMask: require('@/assets/carimages/roadster_top_mask.png'),
+    map: require('@/assets/carimages/roadster_map_base.png'),
+    mapMask: require('@/assets/carimages/roadster_map_mask.png')
   },
   'thinkcity': {
     name: 'Think City',
-    side: require('@/assets/carimages/thinkcity-side-base.png'),
-    sideMask: require('@/assets/carimages/thinkcity-side-mask.png'),
-    top: require('@/assets/carimages/thinkcity-top-base.png'),
-    topMask: require('@/assets/carimages/thinkcity-top-mask.png'),
-    map: require('@/assets/carimages/thinkcity-map-base.png'),
-    mapMask: require('@/assets/carimages/thinkcity-map-mask.png')
+    side: require('@/assets/carimages/thinkcity_side_base.png'),
+    sideMask: require('@/assets/carimages/thinkcity_side_mask.png'),
+    top: require('@/assets/carimages/thinkcity_top_base.png'),
+    topMask: require('@/assets/carimages/thinkcity_top_mask.png'),
+    map: require('@/assets/carimages/thinkcity_map_base.png'),
+    mapMask: require('@/assets/carimages/thinkcity_map_mask.png')
   },
   'twizy': {
     name: 'Renault Twizy',
-    side: require('@/assets/carimages/twizy-side-base.png'),
-    sideMask: require('@/assets/carimages/twizy-side-mask.png'),
-    top: require('@/assets/carimages/twizy-top-base.png'),
-    topMask: require('@/assets/carimages/twizy-top-mask.png'),
-    map: require('@/assets/carimages/twizy-map-base.png'),
-    mapMask: require('@/assets/carimages/twizy-map-mask.png')
+    side: require('@/assets/carimages/twizy_side_base.png'),
+    sideMask: require('@/assets/carimages/twizy_side_mask.png'),
+    top: require('@/assets/carimages/twizy_top_base.png'),
+    topMask: require('@/assets/carimages/twizy_top_mask.png'),
+    map: require('@/assets/carimages/twizy_map_base.png'),
+    mapMask: require('@/assets/carimages/twizy_map_mask.png')
   },
   'volt': {
     name: 'Chevrolet Volt',
-    side: require('@/assets/carimages/volt-side-base.png'),
-    sideMask: require('@/assets/carimages/volt-side-mask.png'),
-    top: require('@/assets/carimages/volt-top-base.png'),
-    topMask: require('@/assets/carimages/volt-top-mask.png'),
-    map: require('@/assets/carimages/volt-map-base.png'),
-    mapMask: require('@/assets/carimages/volt-map-mask.png')
+    side: require('@/assets/carimages/volt_side_base.png'),
+    sideMask: require('@/assets/carimages/volt_side_mask.png'),
+    top: require('@/assets/carimages/volt_top_base.png'),
+    topMask: require('@/assets/carimages/volt_top_mask.png'),
+    map: require('@/assets/carimages/volt_map_base.png'),
+    mapMask: require('@/assets/carimages/volt_map_mask.png')
   }
 }
 
