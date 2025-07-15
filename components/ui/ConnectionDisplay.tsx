b653e96d2440b3ff70b60700658afeb1b3784e82
Adam Arif Webb-Johnson <126368317+JellyJammz@users.noreply.github.com>
2025-07-15 20:59:37 +0800

    Reworked ConnectionDisplay states

--- components/ui/ConnectionDisplay.tsx
+++ components/ui/ConnectionDisplay.tsx
@@ -1,12 +1,17 @@
-import { getConnectionState, VehicleConnectionState } from "@/store/connectionSlice"
+import { getCarConnected, getConnectionState, VehicleConnectionState } from "@/store/connectionSlice"
+import { generateGetMetricValueSelector } from "@/store/metricsSlice"
 import React, { useEffect } from "react"
 import { Icon } from "react-native-paper"
 import Animated, { useAnimatedStyle, useSharedValue, withRepeat, withTiming } from "react-native-reanimated"
 import { useSelector } from "react-redux"
 
 export function ConnectionDisplay(): React.JSX.Element {
-  let color = '#00ff00ff'
-  let flashing = false
+  let color;
+  let flashing;
+
+  const green = "#00ff00ff"
+  const orange = "#ff8000ff"
+  const red = "#ff0000ff"
 
   const source = 'antenna'
   const opacity = useSharedValue(0.7);
@@ -26,16 +31,19 @@ export function ConnectionDisplay(): React.JSX.Element {
   }, []);
 
   const connectionState = useSelector(getConnectionState)
+  const carConnected = useSelector(getCarConnected)
 
-  switch(connectionState) {
-    case VehicleConnectionState.DISCONNECTED:
-    case VehicleConnectionState.ERROR:
-      color = '#ff0000ff'
-      break
-    case VehicleConnectionState.AUTHENTICATING:
-    case VehicleConnectionState.CONNECTING:
+  if(connectionState == VehicleConnectionState.CONNECTED) {
+    if(carConnected) {
+      color = green
+      flashing = false
+    } else {
+      color = orange
       flashing = true
-      break
+    }
+  } else {
+    color = red
+    flashing = true
   }
 
   if(flashing) {
