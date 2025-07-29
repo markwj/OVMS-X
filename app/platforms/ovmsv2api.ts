802f5ee85ff962edbd1a3eb4521847f76d97a5b3
Mark Webb-Johnson <mark@openvehicles.com>
2025-07-29 22:07:05 +0800

    Stub store dispatch

--- app/platforms/ovmsv2api.ts
+++ app/platforms/ovmsv2api.ts
@@ -5,6 +5,7 @@ import { Vehicle } from "@/store/vehiclesSlice";
 import { store } from "@/store/root";
 import { notificationsUniqueID } from "@/store/notificationSlice";
 import { metricsSlice } from "@/store/metricsSlice";
+import { stackRouterOverride } from "expo-router/build/layouts/StackClient";
 
 export class OvmsV2Api extends Platform {
   constructor(vehicle: Vehicle) {
@@ -14,6 +15,7 @@ export class OvmsV2Api extends Platform {
   connect() {
     console.log("[platform OVMSv2Api] connect", this.currentVehicle.name);
     //store.dispatch(metricsSlice.actions.setMetric({ key: 'm.egpio.input', value: "Yo dude!" }));
+    //const dispatch = store.dispatch;
   }
 
   disconnect() {
