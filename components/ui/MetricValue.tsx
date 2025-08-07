bcfe2d4911a2dbde4dd00c2040d1df3742779084
Mark Webb-Johnson <mark@openvehicles.com>
2025-08-07 06:44:38 +0100

    Add support for MetricRecord and MetricVal UI component using that.

--- components/ui/MetricValue.tsx
+++ components/ui/MetricValue.tsx
@@ -7,6 +7,7 @@ import { selectMetricUnit, selectMetricValue, selectMetricIsStale, selectLocalis
 import { store } from '@/store/root'
 import { numericalUnitConvertor } from '../utils/numericalUnitConverter'
 import { useTranslation } from 'react-i18next'
+import { MetricRecord } from '@/components/vehicle/metrics'
 
 type Props = Omit<ComponentProps<typeof Text>, 'children'> & { metricKey: string, addSpace? : boolean, emptyOverride? : string, showUnit? : boolean, toBest? : boolean, abbreviateUnit? : boolean }
 
@@ -34,6 +35,39 @@ export function MetricValue(props : Props) {
   return (
     <Text {...props} style={[props.style, (stale && {opacity: 0.5})]}>
       {value != null ? value + (addSpace ? " " : "") + (showUnit ? (unit ?? "") : "") : (t(props.emptyOverride ?? ""))}
+    </Text>
+  )
+}
+
+type MetricRecordProps = Omit<ComponentProps<typeof Text>, 'children'> & { 
+  metricRecord: MetricRecord | null, 
+  addSpace?: boolean, 
+  emptyOverride?: string, 
+  showUnit?: boolean, 
+  abbreviateUnit?: boolean 
+}
+
+export function MetricVal(props: MetricRecordProps) {
+  const {t} = useTranslation()
+  const theme = useTheme()
+
+  if (!props.metricRecord) {
+    return (
+      <Text {...props} style={props.style}>
+        {t(props.emptyOverride ?? "")}
+      </Text>
+    )
+  }
+
+  const { localisedValue, stale } = props.metricRecord
+
+  let addSpace = props.addSpace ?? !(["%", "°", "°C", "°F"].includes("")) // No unit in MetricRecord, so default to true
+  let showUnit = props.showUnit ?? true
+  let abbreviateUnit = props.abbreviateUnit ?? true
+
+  return (
+    <Text {...props} style={[props.style, (stale && {opacity: 0.5})]}>
+      {localisedValue != null ? localisedValue : (t(props.emptyOverride ?? ""))}
     </Text>
   )
 }
